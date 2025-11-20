78b19f8aec28f0c57598b77c5f12a30ff2763d72
angelamichelleruizlinares-a11y <angelamichelleruizlinares@gmail.com>
2025-11-20 18:57:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Mostrar mensaje cuando carga la página
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página cargada correctamente");
+});
+
+// Función para el botón
+function mostrarMensaje() {
+    alert("Gracias por visitar Dulces Ángela 🍰💕");
+}
